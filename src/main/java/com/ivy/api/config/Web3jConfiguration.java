1cc1b74f4e9721336b059556b2305fe32a1de4f2
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-10-02 00:56:42 +0800

    setup web3j

---
+++ src/main/java/com/ivy/api/config/Web3jConfiguration.java
@@ -0,0 +1,15 @@
+package com.ivy.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+
+@Configuration
+public class Web3jConfiguration {
+    @Bean
+    Web3j web3j(@Value("${web3.rpc_url}") String rpcUrl) {
+        return Web3j.build(new HttpService(rpcUrl));
+    }
+}
