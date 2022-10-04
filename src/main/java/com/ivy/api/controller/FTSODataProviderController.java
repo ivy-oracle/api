1e93dc9e3186d5099a625835497466a77dfca95a
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-10-04 19:24:29 +0800

    add ftso data provider by address endpoint

--- src/main/java/com/ivy/api/controller/FTSODataProviderController.java
+++ src/main/java/com/ivy/api/controller/FTSODataProviderController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class FTSODataProviderController {
     ResponseEntity<List<FTSODataProviderDTO>> getDataProviders() {
         var providers = this.ftsoDataProviderService.getAllFTSODataProviders();
         return ResponseEntity.ok(providers);
+    }
+
+    @GetMapping("/{address}")
+    ResponseEntity<FTSODataProviderDTO> getDataProvider(@PathVariable("address") String address) {
+        var provider = this.ftsoDataProviderService.getFTSODataProvider(address);
+        return ResponseEntity.ok(provider);
     }
 }
--- src/main/java/com/ivy/api/service/FTSODataProviderService.java
+++ src/main/java/com/ivy/api/service/FTSODataProviderService.java
@@ -97,4 +97,60 @@ public class FTSODataProviderService {
 
         return results;
     }
+
+    public FTSODataProviderDTO getFTSODataProvider(String address) {
+        VoterWhitelister voterWhitelister = this.contractService.getVoterWhitelister();
+
+        Tuple2<List<BigInteger>, List<String>> supportedIndicesAndSymbols;
+        try {
+            supportedIndicesAndSymbols = this.contractService.getFtsoRegistry().getSupportedIndicesAndSymbols().send();
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Failed to get supported FTSO indices and symbols", e);
+        }
+
+        Map<String, List<String>> votersAddressMap = new HashMap<String, List<String>>();
+
+        for (String symbol : supportedIndicesAndSymbols.component2()) {
+            try {
+                var votersAddress = voterWhitelister.getFtsoWhitelistedPriceProvidersBySymbol(symbol).send();
+                for (int i = 0; i < votersAddress.size(); i++) {
+                    var voterAddress = votersAddress.get(i);
+                    if (votersAddressMap.containsKey(voterAddress)) {
+                        var updatedWhitelistedSymbols = votersAddressMap.get(voterAddress).stream()
+                                .collect(Collectors.toList());
+                        updatedWhitelistedSymbols.add(symbol);
+                        votersAddressMap.put((String) voterAddress, updatedWhitelistedSymbols);
+                    } else {
+                        votersAddressMap.put((String) voterAddress, List.of(symbol));
+                    }
+                }
+            } catch (Exception e) {
+                throw new ResponseStatusException(
+                        HttpStatus.INTERNAL_SERVER_ERROR, "Failed to get whitelisted voters", e);
+            }
+        }
+
+        RewardEpochDTO rewardEpochDTO = this.ftsoService.getRewardEpoch();
+
+        FTSODataProviderDTO ftsoDataProviderDTO = FTSODataProviderDTO.builder()
+                .address(address)
+                .whitelistedSymbols(votersAddressMap.getOrDefault(address, List.of()))
+                .build();
+        var task = this.executor.submit(
+                new FTSODataProviderResolver(ftsoDataProviderDTO, rewardEpochDTO, this.contractService));
+
+        FTSODataProviderDTO result;
+        try {
+            result = task.get();
+        } catch (ExecutionException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "failed to resolve FTSO data providers", e);
+        } catch (InterruptedException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "failed to resolve FTSO data providers", e);
+        }
+
+        return result;
+    }
 }
