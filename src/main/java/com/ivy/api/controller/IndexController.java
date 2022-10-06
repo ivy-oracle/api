7b2b26189e10f87d55f607db69bd23f88ab0c392
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-10-06 16:31:22 +0800

    configure application properties and profiles

--- src/main/java/com/ivy/api/controller/IndexController.java
+++ src/main/java/com/ivy/api/controller/IndexController.java
@@ -1,14 +1,34 @@
 package com.ivy.api.controller;
 
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+    private final ConfigurableEnvironment configurableEnvironment;
+
+    public IndexController(ConfigurableEnvironment configurableEnvironment) {
+        this.configurableEnvironment = configurableEnvironment;
+    }
 
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok("pong");
     }
+
+    @GetMapping("/profile")
+    public ResponseEntity<String> defaultProfiles() {
+        var activeProfiles = this.configurableEnvironment.getActiveProfiles();
+        if (activeProfiles.length > 0) {
+            return ResponseEntity.ok(activeProfiles[0]);
+        }
+        var defaultProfiles = this.configurableEnvironment.getDefaultProfiles();
+        if (defaultProfiles.length > 0) {
+            return ResponseEntity.ok(defaultProfiles[0]);
+        }
+        return ResponseEntity.ok("no profile set");
+    }
+
 }
