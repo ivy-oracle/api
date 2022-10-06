8d09d8c216161aeb3ba819f13aabbabd4d59b4f7
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-10-06 16:19:08 +0800

    rename ping controller to index controller

--- src/main/java/com/ivy/api/controller/IndexController.java
+++ src/main/java/com/ivy/api/controller/IndexController.java
@@ -2,14 +2,13 @@ package com.ivy.api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("ping")
-public class PingController {
-    @GetMapping()
-    public ResponseEntity<String> index() {
+public class IndexController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
         return ResponseEntity.ok("pong");
     }
 }
--- src/test/java/com/ivy/api/controller/IndexControllerTest.java
+++ src/test/java/com/ivy/api/controller/IndexControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PingControllerTest {
+public class IndexControllerTest {
     @Autowired
     MockMvc mockMvc;
 
