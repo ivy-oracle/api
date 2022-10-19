3683fd2b3a7faa37282fd8c127b7887fa46a3b06
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-10-19 23:01:46 +0800

    configure cors

---
+++ src/main/java/com/ivy/api/config/AppConfiguration.java
@@ -0,0 +1,15 @@
+package com.ivy.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://ivyoracle.xyz")
+                .allowedMethods("*");
+    }
+}
