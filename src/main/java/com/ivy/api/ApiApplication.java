7efa9f934cfaa9f58d12090495dd773e4b750b85
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-10-27 18:04:13 +0800

    enable feign clients

--- src/main/java/com/ivy/api/ApiApplication.java
+++ src/main/java/com/ivy/api/ApiApplication.java
@@ -3,11 +3,13 @@ package com.ivy.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@EnableFeignClients
 public class ApiApplication {
 
 	public static void main(String[] args) {
