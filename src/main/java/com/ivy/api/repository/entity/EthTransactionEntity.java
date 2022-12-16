33fc52e2afbffa46010197c59d38d6854d52a1e8
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-12-16 16:28:34 -0500

    indexer: create EthTransaction entity

---
+++ src/main/java/com/ivy/api/repository/entity/EthTransactionEntity.java
@@ -0,0 +1,61 @@
+package com.ivy.api.repository.entity;
+
+import java.math.BigInteger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "eth_transaction")
+public class EthTransactionEntity {
+    @Id
+    @NotNull
+    @Column(name = "transaction_hash")
+    private String transactionHash;
+
+    @NotNull
+    @Column(name = "from_address")
+    private String fromAddress;
+
+    @NotNull
+    @Column(name = "to_address")
+    private String toAddress;
+
+    @NotNull
+    @Column(name = "value")
+    private BigInteger value;
+
+    @NotNull
+    @Column(name = "gas")
+    private BigInteger gas;
+
+    @NotNull
+    @Column(name = "gas_price")
+    private BigInteger gasPrice;
+
+    @NotNull
+    @Column(name = "nonce")
+    private BigInteger nonce;
+
+    @NotNull
+    @Column(name = "transactionIndex")
+    private BigInteger transactionIndex;
+
+    @ManyToOne
+    @JoinColumn(name = "block_number", referencedColumnName = "block_number")
+    private EthBlockEntity block;
+}
