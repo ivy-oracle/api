1e57685e8e9ca9b8fb9d54b9eb1e1ff4442336a8
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2023-01-01 01:02:41 -0500

    indexer: add eth_address table

--- src/main/resources/db/migration/V2.0__indexer.sql
+++ src/main/resources/db/migration/V2.0__indexer.sql
@@ -4,6 +4,11 @@ CREATE TABLE eth_block (
     block_timestamp TIMESTAMP NOT NULL
 );
 
+CREATE TABLE eth_address (
+    eth_address VARCHAR(50) PRIMARY KEY,
+    is_contract BOOLEAN
+);
+
 CREATE TABLE eth_transaction (
     transaction_hash TEXT PRIMARY KEY,
     from_address VARCHAR(50) NOT NULL,
@@ -14,5 +19,7 @@ CREATE TABLE eth_transaction (
     nonce NUMERIC NOT NULL,
     transaction_index NUMERIC NOT NULL,
     block_number NUMERIC NOT NULL,
-    CONSTRAINT fk_eth_block FOREIGN KEY(block_number) REFERENCES eth_block(block_number)
+    CONSTRAINT fk_eth_block FOREIGN KEY(block_number) REFERENCES eth_block(block_number),
+    CONSTRAINT fk_to_address FOREIGN KEY (to_address) REFERENCES eth_address (eth_address),
+    CONSTRAINT fk_from_address FOREIGN KEY (from_address) REFERENCES eth_address (eth_address)
 );
