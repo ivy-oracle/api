5a545a4033fb52215e78a31ba4129037e5ada6b1
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2023-01-07 20:23:34 -0500

    delegation: fix query amount filter

--- src/main/java/com/ivy/api/repository/DelegationRepository.java
+++ src/main/java/com/ivy/api/repository/DelegationRepository.java
@@ -16,24 +16,24 @@ import com.ivy.api.repository.entity.DelegationEntity;
 @Repository
 @Transactional
 public interface DelegationRepository extends JpaRepository<DelegationEntity, String> {
-    DelegationEntity getByFromAddressAndToAddress(String fromAddress, String toAddress);
+        DelegationEntity getByFromAddressAndToAddress(String fromAddress, String toAddress);
 
-    Page<DelegationEntity> findAllByToAddress(String toAddress, Pageable pageable);
+        Page<DelegationEntity> findAllByToAddress(String toAddress, Pageable pageable);
 
-    Page<DelegationEntity> findAllByFromAddress(String fromAddress, Pageable pageable);
+        Page<DelegationEntity> findAllByFromAddress(String fromAddress, Pageable pageable);
 
-    @Query(value = "select to_address as address, avg(amount) as average, "
-            + "count(*) as count, "
-            + "stddev_pop(amount) as standardDeviation from "
-            + "delegation group by to_address", nativeQuery = true)
-    List<DelegationStatProjection> getStats();
+        @Query(value = "select to_address as address, avg(amount) as average, "
+                        + "count(*) as count, "
+                        + "stddev_pop(amount) as standardDeviation from "
+                        + "delegation where amount >= 1 group by to_address", nativeQuery = true)
+        List<DelegationStatProjection> getStats();
 
-    @Query(value = "select to_address as address, avg(amount) as average, "
-            + "count(*) as count, "
-            + "delegation where to_address = :address group by to_address", nativeQuery = true)
-    DelegationStatProjection getStats(String address);
+        @Query(value = "select to_address as address, avg(amount) as average, "
+                        + "count(*) as count, "
+                        + "delegation where to_address = :address and amount >= 1 group by to_address", nativeQuery = true)
+        DelegationStatProjection getStats(String address);
 
-    @Modifying
-    @Query(value = "refresh materialized view concurrently delegation;", nativeQuery = true)
-    void refreshMaterializedView();
+        @Modifying
+        @Query(value = "refresh materialized view concurrently delegation;", nativeQuery = true)
+        void refreshMaterializedView();
 }
