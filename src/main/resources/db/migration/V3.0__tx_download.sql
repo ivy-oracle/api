c18cfd3ed4bea386197f58b4a3bb0d788c1b058d
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2023-01-17 14:07:25 -0500

    indexer: add index for tx table

--- src/main/resources/db/migration/V3.0__tx_download.sql
+++ src/main/resources/db/migration/V3.0__tx_download.sql
@@ -6,3 +6,9 @@ CREATE TABLE "eth_user" (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT fk_eth_address FOREIGN KEY("address") REFERENCES eth_address(eth_address)
 )
+
+CREATE INDEX eth_transaction_from_address_index ON eth_transaction (from_address);
+
+CREATE INDEX eth_transaction_to_address_index ON eth_transaction (to_address);
+
+CREATE INDEX eth_block_timestamp_index ON eth_block (block_timestamp);
